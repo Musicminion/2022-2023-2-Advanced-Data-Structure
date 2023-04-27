133b4ff9c4144be16612520cc24aeeb90fb25b9a
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2023-04-27 04:44:01 +0000

    作业11:Code

--- 作业11 Lab2 图的最短路/graph.cc
+++ 作业11 Lab2 图的最短路/graph.cc
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<map>
+#include<vector>
+#include<climits>
+#include<queue>
 using namespace std;
 
 
@@ -27,19 +30,79 @@ public:
   }
 };
 
-
 int N, M, K;
 map<int, map<int, Path*> > AllPaths;
+int maxSavedTime = 0;
 
 void readData(){
   cin>>N>>M>>K;
   for(int i=0;i<M;i++){
     int u, v, lower, upper, step;
     cin>>u>>v>>lower>>upper>>step;
+    
+    // 最大能够节约的时间
+    maxSavedTime += upper - lower;
     AllPaths[u][v] = new Path(lower, upper, step);
   }
 }
 
+// 获取从起点到终点的路径的权值
+int getPathTime(int startTime) {
+  // 创建一个vector数组，用来存储每个节点的最快的到达时间
+  vector<int> arriveTime(N, INT_MAX);
+  // 起点的到达时间是startTime
+  arriveTime[0] = startTime;
+
+  // 创建一个队列，用来存储当前正在遍历的节点
+  queue<int> q;
+  q.push(0);
+
+  // 当队列不为空时，说明还有节点没有遍历完
+  while(!q.empty()){
+    // 获取当前正在遍历的节点
+    int cur = q.front();
+    q.pop();
+
+    // 假如当前节点没有邻接节点，那么就跳过
+    if (AllPaths.count(cur) == 0){
+      continue;
+    }
+    // 遍历当前节点的所有邻接节点
+    for(auto it = AllPaths[cur].begin(); it != AllPaths[cur].end(); it++){
+      int next = it->first;
+      int power = it->second->getPathPower(arriveTime[cur]);
+      // 如果从当前节点到达邻接节点的时间比之前的到达时间要短
+      if (arriveTime[next] > arriveTime[cur] + power){
+        // 更新到达时间
+        arriveTime[next] = arriveTime[cur] + power;
+        // 将邻接节点加入队列
+        q.push(next);
+      }
+    }
+  }
+  
+  // 返回终点的到达时间
+  return arriveTime[N-1];
+}
+
+
 int main() {
+  readData();
+  // 遍历不同的出发时间，找到最短的时间
+  int earliestArriveTime = INT_MAX;
+  int departTime = 0;
+  // 我出发的时间是K的倍数，比如0、K、2K、3K...
+  // 路径的权值是周期变化的
+  // 我需要确保我出发的时间不能超过最大能够节约的时间
+  int iLimit = maxSavedTime / K;
+  for (int i = 0; i < iLimit; i++){
+    // 出发时间是 i * K 
+    int curArriveTime = getPathTime(i * K);
+    if (curArriveTime < earliestArriveTime){
+      earliestArriveTime = curArriveTime;
+      departTime = i * K;
+    }
+  }
+  cout<< departTime << " " << earliestArriveTime << endl;
   return 0;
 }
